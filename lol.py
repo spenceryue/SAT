c1325b1fb374a45e27bb6773bb7736e2de911d4e
Spencer Yue <spenceryue@gmail.com>
2018-07-05 04:58:26 -0500

    from before

--- lol.py
+++ lol.py
@@ -1,55 +1,7 @@
 import sys
 import numpy as np
-from functools import total_ordering
 
 
-@total_ordering
-class Node:
-    __slots__ = ['layer_start', 'idx', 'data']
-
-
-
-    def __init__ (self, idx, N):
-        self.layer_start = (idx // 7) * 7
-        self.idx = idx
-        self.data = np.zeros (N)
-
-    @property
-    def predecessors (self):
-        if self.idx > 7:
-            start = self.layer_start
-            return self.data[start-7:start]
-        else:
-            return []
-
-    def __getitem__ (self, key):
-        return self.data[key]
-
-    def __setitem__ (self, key, value):
-        self.data[key] = value
-
-    def __eq__ (self, other):
-        return self.idx == other.idx
-
-    def __lt__ (self, other):
-        return self.idx < other.idx
-
-    def is_valid (self):
-        if self.idx < 7:
-            return True
-        else:
-            result = True
-            predecessors = self.predecessors
-            while predecessors:
-                result &= any (predecessors)
-                predecessors = predecessors[0]
-
-class Layer:
-    __slots__ = ['slice', 'next', 'data']
-
-    def __init__ (self, data):
-        self.data = data
-        self.
 
 
 def solve (rep):
@@ -57,18 +9,60 @@ def solve (rep):
 
     return result
 
-def parse (input):
-    rep = []
+def parse (f):
+    result = []
+    problem = False
 
-    return rep
+    for line in f:
+        tokens = line.split (' ')
+        for e in tokens:
+            if e not in {'p', 'c', '0'}:
+                result.append (e.strip ())
+            elif e == 'p':
+                problem = True
+                assert result == []
+            elif e == 'c':
+                continue
+            elif e == '0':
+                if problem:
+                    yield tuple (result[1:])
+                else:
+                    yield (int (x) for x in result)
+            else:
+                raise Exception ('Bad input token: {}.'.format (e))
+
+def valid_permutations (clause):
+    """Expects an iterator of three integers representing a 3CNF clause
+    as input.
+    Yields 7 valid permutations by negating components."""
+    clause = list (clause)
+    yield {-x if i & 2**j else x for i in range (7) for j, x in enumerate
+           (clause)}
+
+class Node:
+    def __init__ (i, j, expr, N):
+        self.i, self.j = i, j
+        self.expr = {abs (x): x>0 for x in expr}
+        self.layer = np.ones (N, 7)
+
+    def
+
 
 if __name__ == '__main__':
 
-    if len (sys.argv) > 1:
-        with open (sys.argv[1], 'r') as f:
-            rep = parse (f)
-    else:
-        rep = parse (sys.stdin)
+    if len (sys.argv) <= 1:
+        raise Exception ('Need input file as argument.')
+
+    node = []
+    with open (sys.argv[1], 'r') as f:
+        for i, clause in enumerate (parse (f)):
+            if len (clause) == 2:
+                N, P = clause
+                continue
+            row = []
+            for j, expr in enumerate (valid_permutations (clause)):
+                row.append (Node (i, j, expr, N))
+            node.append (row)
 
     result = solve (rep)
     print (result)
